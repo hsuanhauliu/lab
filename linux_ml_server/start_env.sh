6b6859296a1133180ad6e7b0b66b70039867b334
Howard Liu <23202168+hsuanhauliu@users.noreply.github.com>
2025-04-01 23:12:54 -0700

    chore: add jupyter notebook setup

---
+++ linux_ml_server/start_env.sh
@@ -0,0 +1,6 @@
+# Command to start the jupyter notebook environment
+# - port 8080 is mapped to jupyter notebook UI
+# - port 8081 is mapped to tensorboard
+# - ./data dir is mapped to the base directory Jupyter is using
+docker run --gpus all -d -it -p 8080:8888 -p 8081:6006 -v $(pwd)/data:/home/jovyan -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes --user root --name jupyter_env cschranz/gpu-jupyter:v1.5_cuda-11.6_ubuntu-20.04
+
